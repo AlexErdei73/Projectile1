6148b0fe8931a0657a41764ae946f663d40b9a24
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-23 12:06:01 +0000

    Add improveFinalResult method to Projectile class

--- src/Projectile.java
+++ src/Projectile.java
@@ -2,9 +2,12 @@ public class Projectile {
     private double y;
     public double vy;
     public double t;
+    private Plot plot;
     Projectile(double y, double vy) {
         this.y = y;
         this.vy = vy;
+        this.plot = new Plot ("Height of horizontal projectile in time", 0, 2, 0.5, 0, 10, 0.5);
+        //this.plot.setPointSize(1);
     }
 
     public void simulate() {
@@ -14,13 +17,21 @@ public class Projectile {
       t = 0;
       ay = -9.8;
       t = 0;
-      dt = 0.001;
+      dt = 0.1;
       while (y > 0) {
           t += dt;
           y += vy * dt;
           vy += ay * dt;
+          plot.addPoint(t, y);
       }
       this.t = t;
       this.vy = vy;
+      this.y = y;
+      improveFinalResult();
+    }
+    private void improveFinalResult() {
+        double delta_t = this.y / this.vy;
+        this.t -= delta_t;
+        this.y -= this.vy*delta_t;
     }
 }
