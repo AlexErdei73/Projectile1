71f0365e3b33bd8abe9d1f9e505b19b480c5e07d
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-23 02:04:10 +0000

    Add Projectile and simulate method

---
+++ src/Projectile.java
@@ -0,0 +1,26 @@
+public class Projectile {
+    private double y;
+    public double vy;
+    public double t;
+    Projectile(double y, double vy) {
+        this.y = y;
+        this.vy = vy;
+    }
+
+    public void simulate() {
+      double y, vy, t, dt, ay;
+      y = this.y;
+      vy = this.vy;
+      t = 0;
+      ay = -9.8;
+      t = 0;
+      dt = 0.1;
+      while (y > 0) {
+          t += dt;
+          y += vy * dt;
+          vy += ay * dt;
+      }
+      this.t = t;
+      this.vy = vy;
+    }
+}
--- src/Projectile1.java
+++ src/Projectile1.java
@@ -1,5 +1,8 @@
 public class Projectile1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Projectile projectile = new Projectile(10, 0);
+        projectile.simulate();
+        System.out.println("t = " + projectile.t);
+        System.out.println("vy = " + projectile.vy);
     }
 }
