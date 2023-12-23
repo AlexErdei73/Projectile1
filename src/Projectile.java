dae2b702ab7bd49bc6a1a4a036b87a52dadbb64f
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-23 14:59:39 +0000

    Add air resistance to the app

--- src/Projectile.java
+++ src/Projectile.java
@@ -2,26 +2,32 @@ public class Projectile {
     private double y;
     public double vy;
     public double t;
+    private double drag;
     private Plot plot;
-    Projectile(double y, double vy) {
+    Projectile(double y, double vy, double drag) {
         this.y = y;
         this.vy = vy;
+        this.drag = drag;
         this.plot = new Plot ("Height of horizontal projectile in time", 0, 2, 0.5, 0, 10, 0.5);
         this.plot.setPointSize(1);
     }
 
     public void simulate() {
-      double y, vy, t, dt, ay, delta_t;
+      double y, vy, t, dt, g, ay, delta_t;
       y = this.y;
       vy = this.vy;
       t = 0;
-      ay = -9.8;
+      g =9.8;
       t = 0;
       dt = 0.0001;
       delta_t = 0.001;
       while (y > 0) {
           t += dt;
           y += vy * dt;
+          ay = -g;
+          if (vy != 0) {
+              ay += - this.drag * vy * vy * vy / Math.abs(vy);
+          }
           vy += ay * dt;
           if (t > this.t + delta_t) {
               plot.addPoint(t, y);
