3ba7c714246eba1ad8cad078deb6d544bf98ec48
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-23 13:28:07 +0000

    Add delta_t tp simulate method to add points to plot

--- src/Projectile.java
+++ src/Projectile.java
@@ -7,22 +7,26 @@ public class Projectile {
         this.y = y;
         this.vy = vy;
         this.plot = new Plot ("Height of horizontal projectile in time", 0, 2, 0.5, 0, 10, 0.5);
-        //this.plot.setPointSize(1);
+        this.plot.setPointSize(1);
     }
 
     public void simulate() {
-      double y, vy, t, dt, ay;
+      double y, vy, t, dt, ay, delta_t;
       y = this.y;
       vy = this.vy;
       t = 0;
       ay = -9.8;
       t = 0;
-      dt = 0.1;
+      dt = 0.0001;
+      delta_t = 0.001;
       while (y > 0) {
           t += dt;
           y += vy * dt;
           vy += ay * dt;
-          plot.addPoint(t, y);
+          if (t > this.t + delta_t) {
+              plot.addPoint(t, y);
+              this.t = t;
+          }
       }
       this.t = t;
       this.vy = vy;
