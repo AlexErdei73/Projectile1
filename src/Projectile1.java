271e342a0c179ab25fb7ce483d1260b9050f0c92
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-24 10:14:31 +0000

    Format numbers to three decimal digits

--- src/Projectile1.java
+++ src/Projectile1.java
@@ -1,8 +1,11 @@
+import java.text.DecimalFormat;
+
 public class Projectile1 {
     public static void main(String[] args) {
         Projectile projectile = new Projectile(10, 0, 0.1);
         projectile.simulate();
-        System.out.println("t = " + projectile.t);
-        System.out.println("vy = " + projectile.vy);
+        DecimalFormat threeDigits = new DecimalFormat("0.000");
+        System.out.println("t = " + threeDigits.format(projectile.t));
+        System.out.println("vy = " + threeDigits.format(projectile.vy));
     }
 }
