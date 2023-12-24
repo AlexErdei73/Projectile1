90d36b1dc09d3421135900492553b2f9f417f7a9
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-24 10:08:06 +0000

    Use Euler-Richardson algorithm

--- src/Projectile.java
+++ src/Projectile.java
@@ -15,22 +15,21 @@ public class Projectile {
     }
 
     public void simulate() {
-      double y, vy, t, dt, g, ay, delta_t;
+      double y, vy, t, dt, ay, delta_t, ymid, vymid, aymid;
       y = this.y;
       vy = this.vy;
       t = 0;
-      g =9.8;
       t = 0;
       dt = 0.0001;
       delta_t = 0.001;
       while (y > 0) {
+          ay = this.ay(vy);
+          ymid = y + vy * 0.5 * dt;
+          vymid = vy + ay * 0.5 * dt;
+          aymid = this.ay(vymid);
+          y += vymid * dt;
+          vy += aymid * dt;
           t += dt;
-          y += vy * dt;
-          ay = -g;
-          if (vy != 0) {
-              ay += - this.drag * vy * vy * vy / Math.abs(vy);
-          }
-          vy += ay * dt;
           if (t > this.t + delta_t) {
               plot.setColor(Color.red);
               plot.addPoint(t, y);
@@ -43,6 +42,11 @@ public class Projectile {
       this.vy = vy;
       this.y = y;
       improveFinalResult();
+    }
+    private double ay(double vy) {
+        double g =9.8;
+        if (vy == 0) return -g;
+        else return -g - this.drag * vy * vy * vy / Math.abs(vy);
     }
     private void improveFinalResult() {
         double delta_t = this.y / this.vy;
--- src/Projectile1.java
+++ src/Projectile1.java
@@ -1,6 +1,6 @@
 public class Projectile1 {
     public static void main(String[] args) {
-        Projectile projectile = new Projectile(10, 0, 1);
+        Projectile projectile = new Projectile(10, 0, 0.1);
         projectile.simulate();
         System.out.println("t = " + projectile.t);
         System.out.println("vy = " + projectile.vy);
